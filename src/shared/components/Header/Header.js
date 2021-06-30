afabf1b96340f995856bf7b29498cdef2eb60a2e
Ihor Bohdanov <gosha0990dnepr@gmail.com>
2021-06-30 20:27:58 +0300

    initial commit

---
+++ src/shared/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import "./Header.scss";
+
+export function Header(props) {
+  return (
+    <div className="example">
+      This is an example component
+    </div>
+  );
+}
+
+Header.propTypes = {};
